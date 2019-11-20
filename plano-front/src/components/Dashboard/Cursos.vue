84aa09d73798a5e07ff0f3a8be184eb51b479701
Vitoria <vitorianatalia@ice.ufjf.br>
2019-11-20 19:22:09 -0300

    correção de erro de merge em cursos

--- plano-front/src/components/Dashboard/Cursos.vue
+++ plano-front/src/components/Dashboard/Cursos.vue
@@ -73,11 +73,6 @@
      <div class="cartao-inteiro col-lg-5 col-md-12 col-sm-12 col-12 mt-3 pl-0 ml-auto">
       <div class="col card cartao ml-auto"style="margin-right:20px; max-width: 350px;">
         <div class="card-body" style="padding:20px; overflow-y:auto; overflow-x:hidden;">
-      <div
-        class="col col-lg-12 col-md-12 col-sm-12 col-12 card cartao"
-        style="width: 100%; max-width: 350px; max-height: 79vh;"
-      >
-        <div class="card-body">
           <div
             class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-2 pb-2 mb-3 border-bottom"
           >
