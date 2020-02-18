bbd634bfd0319256fe60628cb8609701a9e489b9
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-02-18 16:16:37 -0300

    update modal buttons Horarios

--- plano-front/src/components/Dashboard/Horarios.vue
+++ plano-front/src/components/Dashboard/Horarios.vue
@@ -178,7 +178,7 @@
         </table>
       </div>
       <div slot="modal-footer" class="w-100 m-0" style="display: flex">
-        <div class="row ml-2 w-100">
+        <div class="row ml-0 w-100">
           <b-button
             class="btn-azul btn-df mr-2"
             variant="success"
