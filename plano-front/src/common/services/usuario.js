0b3940a78d3cabd5bd5f93c0f4431fe918dce78b
lucascribeiro <lucascarvalho@ice.ufjf.br>
2020-06-30 14:14:41 -0300

    Criação de funções para deletar e listar usuários

--- plano-front/src/common/services/usuario.js
+++ plano-front/src/common/services/usuario.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 
 export default {
+  fetchAll (config = {}) {
+    return Vue.axios.get(`/usuario`, config)
+        .then((response) => Promise.resolve(response.data))
+        .catch((error) => Promise.reject(error))
+  },
+
   create (payload, config = {}) {
     return Vue.axios.post('/usuario', payload, config)
-      .then((response) => Promise.resolve(response.data))
-      .catch((error) => Promise.reject(error))
+        .then((response) => Promise.resolve(response.data))
+        .catch((error) => Promise.reject(error))
   },
 
   update (userId, config = {}) {
     return Vue.axios.post(`/usuario/${userId}`, config)
-      .then((response) => Promise.resolve(response.data))
-      .catch((error) => Promise.reject(error))
+        .then((response) => Promise.resolve(response.data))
+        .catch((error) => Promise.reject(error))
   },
+
+  delete (userId, config = {}) {
+    return Vue.axios.delete(`/usuario/${userId}`, config)
+        .then((response) => Promise.resolve(response.data))
+        .catch((error) => Promise.reject(error))
+  }
 }
