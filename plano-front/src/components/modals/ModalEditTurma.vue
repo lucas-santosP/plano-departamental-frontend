2f6de26228be7e55c9db4974e984dbf76d3e9af1
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-08-04 18:03:48 -0300

    Corrige nome de ref em ModalEditTurma

--- plano-front/src/components/modals/ModalEditTurma.vue
+++ plano-front/src/components/modals/ModalEditTurma.vue
@@ -1,5 +1,5 @@
 <template>
-  <BaseModal2 ref="baseModalRelatorio" type="editTurma" title="Edição de Turma">
+  <BaseModal2 ref="baseModalEditTurma" type="editTurma" title="Edição de Turma">
     <template #modal-body v-if="turmaSelected">
       <BodyModalEditTurma
         :hasEditDisciplina="hasEditDisciplina"
@@ -22,10 +22,10 @@ export default {
   },
   methods: {
     open() {
-      this.$refs.baseModalRelatorio.open();
+      this.$refs.baseModalEditTurma.open();
     },
     close() {
-      this.$refs.baseModalRelatorio.close();
+      this.$refs.baseModalEditTurma.close();
     },
   },
 };
