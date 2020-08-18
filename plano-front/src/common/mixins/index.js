2f4d5b04a898fa15bba457d3d080efdf143a4891
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-08-18 18:38:32 -0300

    feat: Adiciona mixins para gerar texto de docentes e para horarios

--- plano-front/src/common/mixins/index.js
+++ plano-front/src/common/mixins/index.js
@@ -4,8 +4,12 @@ import toggleOrdination from "./toggleOrdination";
 import debounceInput from "./debounceInput";
 import maskTurmaLetra from "./maskTurmaLetra";
 import maskOnlyNumber from "./maskOnlyNumber";
+import generateHorariosText from "./generateHorariosText";
+import generateDocentesText from "./generateDocentesText";
 
 export {
+  generateDocentesText,
+  generateHorariosText,
   maskOnlyNumber,
   maskTurmaLetra,
   notification,
