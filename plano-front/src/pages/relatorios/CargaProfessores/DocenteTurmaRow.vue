4e730410fada6641afbe1731aee2aa6007d18672
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-09-21 19:35:08 -0300

    fix: Corrige exibicao de 0 quando input de pedidos estiver vazio

--- plano-front/src/pages/relatorios/CargaProfessores/DocenteTurmaRow.vue
+++ plano-front/src/pages/relatorios/CargaProfessores/DocenteTurmaRow.vue
@@ -6,8 +6,8 @@
     <v-td width="300" align="start">{{ turma.disciplina.nome }}</v-td>
     <v-td width="35" paddingX="2">{{ turma.letra }}</v-td>
 
-    <v-td width="130"
-      >{{ generateHorariosText(turma.Horario1, turma.Horario2) }}
+    <v-td width="130">
+      {{ generateHorariosText(turma.Horario1, turma.Horario2) }}
     </v-td>
 
     <v-td v-if="semestresAtivados.primeiro" width="35" paddingX="2">
