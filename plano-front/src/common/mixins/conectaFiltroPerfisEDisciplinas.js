864afb1331d8a67e7b044c7c0118f5bae9bc6a79
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-10-10 11:59:36 -0300

    refactor: Removendo comentarios

--- plano-front/src/common/mixins/conectaFiltroPerfisEDisciplinas.js
+++ plano-front/src/common/mixins/conectaFiltroPerfisEDisciplinas.js
@@ -1,7 +1,7 @@
 export default {
   methods: {
     selectDisciplina(disciplina) {
-      this.toggleItemInArray(disciplina, this.filtroDisciplinas.selecionados);
+      this.toggleItemInArray(disciplina, this.filtroDisciplinas.selecionados, "id");
       this.conectaDisciplinasEmPerfis();
     },
     conectaDisciplinasEmPerfis() {
@@ -13,20 +13,12 @@ export default {
     },
 
     selectPerfis(perfil) {
-      const indexDoPerfil = this.$_.findIndex(this.filtroPerfis.selecionados, [
-        "id",
-        perfil.id,
-      ]);
-
       perfil.halfChecked = false;
-
-      if (indexDoPerfil === -1) this.filtroPerfis.selecionados.push(perfil);
-      else this.filtroPerfis.selecionados.splice(indexDoPerfil, 1);
+      this.toggleItemInArray(perfil, this.filtroPerfis.selecionados, "id");
 
       this.conectaPerfisEmDisciplinas();
     },
     conectaPerfisEmDisciplinas() {
-      //Se não tem nenhum perfil selecionado, zera disciplinas
       if (!this.filtroPerfis.selecionados.length) {
         this.filtroDisciplinas.selecionados = [];
         return;
@@ -37,9 +29,8 @@ export default {
           "id",
           perfil.id,
         ]);
-        //Se o perfil esta selecionado
+
         if (perfilFounded) {
-          //E se não esta half checked. Então adiciona todas disciplinas do perfil
           if (!perfilFounded.halfChecked) {
             const disciplinasDoPerfil = this.$_.filter(this.DisciplinasOptions, [
               "Perfil",
@@ -52,7 +43,6 @@ export default {
             );
           }
         } else {
-          //Se o perfil não esta selecionado, remove todos as disciplinas desse perfil
           this.filtroDisciplinas.selecionados = this.$_.filter(
             this.filtroDisciplinas.selecionados,
             (disciplina) => disciplina.Perfil !== perfil.id
