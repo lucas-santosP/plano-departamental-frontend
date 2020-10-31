8718f18dba9a75e16124609ef40cd4087ba8259f
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-10-31 17:02:16 -0300

    refactor(Mixins): Melhorando opcoes de mixins

--- plano-front/src/common/utils/normalizeText.js
+++ plano-front/src/common/utils/normalizeText.js
@@ -1,9 +1,11 @@
-function normalizeText(text) {
-  return text
+function normalizeText(text, upperCase = true) {
+  text
     .toUpperCase()
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
     .replace(/\s/g, "");
+
+  return upperCase ? text.toUpperCase() : text;
 }
 
 export default normalizeText;
