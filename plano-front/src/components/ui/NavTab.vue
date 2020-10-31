46306db973cdb68a7239ff651aef58939efec58a
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-10-31 17:04:31 -0300

    feat & refactor(NavTab): Refatoracao adicionando scroll e botoes

--- plano-front/src/components/ui/NavTab.vue
+++ plano-front/src/components/ui/NavTab.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="nav-container">
-    <ul class="nav nav-tabs card-header-tabs m-0" ref="nav">
+    <ul class="nav nav-tabs card-header-tabs m-0" ref="navTab">
+      <button
+        v-if="withScroll"
+        v-show="buttonsVisibility.left"
+        class="btn-slide btn-slide-left"
+        @click="scrollTo('left')"
+      >
+        <font-awesome-icon :icon="['fas', 'arrow-left']" />
+      </button>
+
       <li
-        v-for="tab in allTabs"
-        :key="'navTab' + tab"
-        @click="$emit('change-tab', tab)"
+        v-for="(tab, index) in allTabs"
+        :key="index + tab"
         class="nav-item"
+        @click="$emit('change-tab', tab)"
       >
         <a
           class="nav-link border border-right-0"
-          :class="[
-            'clickable',
-            {
-              active: currentTab === tab,
-            },
-          ]"
+          :class="['clickable', { active: currentTab === tab }]"
         >
-          {{ normalizeText(tab) }}
+          {{ tab }}
         </a>
       </li>
-      <button class="btn-slide" @click="slideRight">
+
+      <button
+        v-if="withScroll"
+        v-show="buttonsVisibility.right"
+        class="btn-slide btn-slide-right"
+        @click="scrollTo('right')"
+      >
         <font-awesome-icon :icon="['fas', 'arrow-right']" />
       </button>
     </ul>
@@ -32,22 +42,53 @@ export default {
   props: {
     currentTab: { type: String, required: true },
     allTabs: { type: Array, required: true },
+    withScroll: { type: Boolean, default: false },
   },
   data() {
     return {
-      trackWidth: null,
+      currentScrollPosition: 0,
+      buttonsVisibility: {
+        left: false,
+        right: true,
+      },
     };
   },
+
   mounted() {
-    this.trackWidth = this.$refs.nav.scrollWidth;
+    this.$refs.navTab.handleScroll = () => {
+      this.currentScrollPosition = this.$refs.navTab.scrollLeft;
+    };
+    this.$refs.navTab.addEventListener("scroll", this.$refs.navTab.handleScroll);
   },
+  beforeDestroy() {
+    this.$refs.navTab.removeEventListener("scroll", this.$refs.navTab.handleScroll);
+  },
+
   methods: {
-    normalizeText(tabText) {
-      if (tabText === "Laboratorios") return "Laborátorios";
-      return tabText;
+    scrollTo(direction) {
+      const scrollValue = this.$refs.navTab.offsetWidth / 2;
+
+      if (direction === "right") this.$refs.navTab.scrollBy(scrollValue, 0);
+      else this.$refs.navTab.scrollBy(-scrollValue, 0);
     },
-    slideRight() {
-      this.$refs.nav.scrollBy(this.trackWidth / 4, 0);
+  },
+
+  watch: {
+    currentScrollPosition(currentScrollPosition) {
+      const navWidth = this.$refs.navTab.offsetWidth;
+      const navMaxScroll = this.$refs.navTab.scrollWidth;
+
+      if (navWidth + currentScrollPosition >= navMaxScroll && navWidth != 0) {
+        this.buttonsVisibility.right = false;
+      } else {
+        this.buttonsVisibility.right = true;
+      }
+
+      if (currentScrollPosition <= 0) {
+        this.buttonsVisibility.left = false;
+      } else {
+        this.buttonsVisibility.left = true;
+      }
     },
   },
 };
@@ -55,10 +96,7 @@ export default {
 
 <style scoped>
 .nav-container {
-  height: 30px;
   width: 100%;
-  padding: 0;
-  margin: 0;
   overflow: hidden;
   position: relative;
 }
@@ -67,34 +105,56 @@ export default {
   height: 30px;
   flex-wrap: nowrap;
   overflow-x: auto;
+  overflow-y: hidden;
   scroll-behavior: smooth;
   scrollbar-color: transparent transparent;
 }
-
 .nav-tabs::-webkit-scrollbar {
   width: 0px;
   height: 0px;
   background: transparent;
 }
 
+.btn-slide {
+  position: absolute;
+  transform: translateY(-50%);
+  top: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 28px;
+  height: 30px;
+
+  border-radius: 5px;
+  border: 2px solid #909090;
+  color: #313131;
+  background-color: var(--light-gray);
+  font-size: 11px;
+  cursor: pointer;
+  box-shadow: none !important;
+  transition: 200ms all ease;
+  box-shadow: none !important;
+}
+
+.btn-slide:hover {
+  background-color: #f8f8f8;
+  color: #000;
+  box-shadow: 0px 0px 20px 5px #50505071 !important;
+}
+
+.btn-slide-right {
+  right: 0;
+}
+.btn-slide-left {
+  left: 0;
+}
+
 .nav-item {
   scroll-snap-align: start;
   -webkit-overflow-scrolling: touch;
 }
-.btn-slide {
-  top: 50%;
-  right: 0;
-  width: 25px;
-  height: 25px;
-  border-radius: 15px;
-  border: 1px black solid;
-  background-color: #fff;
-  transform: translateY(-50%);
-  cursor: pointer;
-  position: absolute;
-}
 .nav-tabs .nav-link {
-  color: #0079fa !important;
+  color: var(--light-blue) !important;
   cursor: pointer;
   user-select: none;
 }
