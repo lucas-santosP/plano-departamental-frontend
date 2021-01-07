5c25f8d763af22f42b8c6c7aac7352ed7a6713b9
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-07 12:02:03 -0300

    fix(mixins): Corrige acesso de variavel de event no maskTurmaLetra

--- plano-front/src/common/mixins/maskTurmaLetra.js
+++ plano-front/src/common/mixins/maskTurmaLetra.js
@@ -1,7 +1,7 @@
 export default {
   methods: {
     maskTurmaLetra($event) {
-      let key = event.key ? $event.key.toUpperCase() : $event.which;
+      let key = $event.key ? $event.key.toUpperCase() : $event.which;
       if (!key.match(/[A-Z]/i) || $event.target.value.length >= 3) $event.preventDefault();
     },
   },
