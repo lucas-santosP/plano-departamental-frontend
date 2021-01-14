68dfc9b1475da55030137e3b301340e5d69a87d2
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-13 21:05:29 -0300

    refactor(Navbar): Utiliza o componente Logo

--- plano-front/src/components/layout/TheNavbar.vue
+++ plano-front/src/components/layout/TheNavbar.vue
@@ -2,8 +2,7 @@
   <nav class="navbar-container shadow">
     <div @click="closeSidebar" class="navbar-brand">
       <router-link :to="{ path: '/home' }" class="brand-title">
-        <font-awesome-icon :icon="['fas', 'graduation-cap']" />
-        SIPlanWeb
+        <Logo />
       </router-link>
     </div>
 
@@ -43,8 +42,10 @@
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import { Logo } from "@/components/ui";
 export default {
   name: "TheNavbar",
+  components: { Logo },
   props: {
     modalCallbacks: { type: Object, required: true },
   },
