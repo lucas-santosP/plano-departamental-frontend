dcd5caaef19e1bca4fcf48603a84b0f2bcfa07e7
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-29 17:18:48 -0300

    feat(LoadingView): Adiciona novo tipo de loading: progress

--- plano-front/src/components/layout/LoadingView.vue
+++ plano-front/src/components/layout/LoadingView.vue
@@ -1,18 +1,21 @@
 <template>
   <transition name="fade-transition">
     <div
-      v-if="onLoading.fetching || onLoading.partial || onLoading.table"
+      v-if="onLoading.fetching || onLoading.partial || onLoading.table || onLoading.progress"
       class="container-loading"
       @click.stop
       @keydown.stop
       @keypress.stop
     >
-      <div class="lds-ring">
+      <div class="loading-spin">
         <div></div>
         <div></div>
         <div></div>
         <div></div>
       </div>
+      <span v-if="onLoading.progress" class="progress-text">
+        {{ loadingProgress.currentPercentage }}%
+      </span>
     </div>
   </transition>
 </template>
@@ -22,7 +25,7 @@ import { mapGetters } from "vuex";
 export default {
   name: "LoadingView",
   computed: {
-    ...mapGetters(["onLoading"]),
+    ...mapGetters(["onLoading", "loadingProgress"]),
   },
 };
 </script>
@@ -42,11 +45,11 @@ export default {
   backdrop-filter: blur(0.5px);
   cursor: wait;
 
-  .lds-ring {
+  .loading-spin {
     position: fixed;
     display: inline-block;
-    width: 64px;
-    height: 64px;
+    width: 70px;
+    height: 70px;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
@@ -56,11 +59,11 @@ export default {
       position: absolute;
       top: 0;
       left: 0;
-      width: 64px;
-      height: 64px;
+      width: 70px;
+      height: 70px;
       border: 8px solid $clr-darkgray;
       border-radius: 50%;
-      animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+      animation: loading-spining 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
       border-color: $clr-darkgray transparent transparent transparent;
       &:nth-child(1) {
         animation-delay: -0.45s;
@@ -73,9 +76,21 @@ export default {
       }
     }
   }
+
+  .progress-text {
+    font-weight: bold;
+    transition: all ease 200ms;
+    font-size: 15px;
+    position: absolute;
+    color: #fff;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-shadow: 2px 2px 2px #000, 0 0 25px #000, 0 0 5px #000;
+  }
 }
 
-@keyframes lds-ring {
+@keyframes loading-spining {
   0% {
     transform: rotate(0deg);
   }
--- plano-front/src/store/module/loadingView.js
+++ plano-front/src/store/module/loadingView.js
@@ -1,11 +1,21 @@
 import Vue from "vue";
-import { SET_LOADING } from "../mutation-types";
+import {
+  SET_LOADING,
+  SET_PROGRESS_BAR,
+  INCREMENT_PROGRESS_BAR_CURRENT_VALUE,
+} from "../mutation-types";
 
 const state = {
   onLoading: {
     partial: false,
     table: false,
     fetching: false,
+    progress: false,
+  },
+  loadingProgress: {
+    currentPercentage: 0,
+    currentValue: 0,
+    finalValue: null,
   },
 };
 
@@ -13,22 +23,55 @@ const mutations = {
   [SET_LOADING](state, data) {
     Vue.set(state.onLoading, data.type, data.value);
   },
+  [SET_PROGRESS_BAR](state, data) {
+    Vue.set(state.loadingProgress, data.key, data.value);
+  },
+  [INCREMENT_PROGRESS_BAR_CURRENT_VALUE](state) {
+    state.loadingProgress.currentValue++;
+  },
 };
 
 const actions = {
   setLoading({ commit }, { type, value }) {
-    if (type === "partial" || type === "table") {
-      if (value) commit(SET_LOADING, { type, value });
-      else setTimeout(() => commit(SET_LOADING, { type, value }), 300); //Tempo mínimo de espera
-    } else if (type == "fetching") {
+    if (type === "fetching") {
       commit(SET_LOADING, { type, value });
+    } else {
+      if (value) commit(SET_LOADING, { type, value });
+      else setTimeout(() => commit(SET_LOADING, { type, value }), 300); // Tempo mínimo de espera
     }
   },
+
+  initializeProgressBar({ commit }, { finalValue }) {
+    commit(SET_LOADING, { type: "progress", value: true });
+    commit(SET_PROGRESS_BAR, { key: "currentValue", value: 0 });
+    commit(SET_PROGRESS_BAR, { key: "finalValue", value: finalValue });
+    commit(SET_PROGRESS_BAR, { key: "currentPercentage", value: 0 });
+  },
+
+  updateProgressBar({ state, commit }) {
+    commit(INCREMENT_PROGRESS_BAR_CURRENT_VALUE);
+
+    const { currentValue, finalValue } = state.loadingProgress;
+    const currentPercentage = Math.floor(currentValue * (100 / finalValue));
+    commit(SET_PROGRESS_BAR, { key: "currentPercentage", value: currentPercentage });
+  },
+
+  async finishProgressBar({ commit }) {
+    await timeout(500);
+    commit(SET_LOADING, { type: "progress", value: false });
+  },
+};
+
+const timeout = (wait) => {
+  return new Promise((resolve) => setTimeout(resolve, wait));
 };
 
 const getters = {
   onLoading(state) {
     return state.onLoading;
+  },
+  loadingProgress(state) {
+    return state.loadingProgress;
   },
 };
 
