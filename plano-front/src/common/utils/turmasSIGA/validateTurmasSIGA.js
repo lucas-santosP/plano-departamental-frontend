8ef1d0e394e1d1daca441b93ea153038b072a404
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-02-01 17:13:45 -0300

    fix(Utils): Corrige validacao de turmasSIGA

--- plano-front/src/common/utils/turmasSIGA/validateTurmasSIGA.js
+++ plano-front/src/common/utils/turmasSIGA/validateTurmasSIGA.js
@@ -1,10 +1,18 @@
-import { getKeysTurmaSIGA } from "./index";
-
 export default function validateTurmasSIGA(turmasSIGA) {
   if (!turmasSIGA.length) return;
 
-  const keysTurmaSIGA = getKeysTurmaSIGA(turmasSIGA);
-  if (Object.keys(turmasSIGA[0]).length < Object.keys(keysTurmaSIGA).length) {
+  const expectedKeys = [
+    "DISCIPLINA",
+    "DISCIPLINA_COD.",
+    "TURMA",
+    "CURSO_COD.",
+    "CURSO",
+    "VAGASOFERECIDAS",
+    "VAGASOCUPADAS",
+    "HORARIOS",
+    "DOCENTES",
+  ];
+  if (Object.keys(turmasSIGA[0]).length < expectedKeys.length) {
     throw new Error("Verifique a integridade e o formato do(s) arquivo(s)");
   }
 }
