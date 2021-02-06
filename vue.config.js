88546f14ea81f3aec3836488adb881a48e288d78
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-02-06 11:34:17 -0300

    refactor: Adiciona path alias de utils e mixins

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@utils": path.resolve(__dirname, "src/common/utils"),
+        "@mixins": path.resolve(__dirname, "src/common/mixins"),
+      },
+    },
+  },
+};
