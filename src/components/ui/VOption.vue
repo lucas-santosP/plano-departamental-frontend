2272cd32a3ee8111ef638a183defbb7e1c615e16
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-02-15 16:42:10 -0300

    feat(VOption): Cria component de option para VSelect

---
+++ src/components/ui/VOption.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-form-select-option :value="value" :disabled="disabled">{{ text }}</b-form-select-option>
+</template>
+
+<script>
+export default {
+  name: "VOption",
+  props: {
+    value: {
+      default: "",
+      validator: (prop) => {
+        return (
+          typeof prop === "string" ||
+          typeof prop === "number" ||
+          typeof prop === "object" ||
+          prop === null
+        );
+      },
+    },
+    text: { type: String, required: true },
+    disabled: { type: Boolean, default: false },
+  },
+};
+</script>
