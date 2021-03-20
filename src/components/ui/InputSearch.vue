efd75d611ad6abb8c6f50836e6af686f288f26c4
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-03-20 19:58:08 -0300

    refactor(InputSearch): Padronizando utilizando o b-form-input

--- src/components/ui/InputSearch.vue
+++ src/components/ui/InputSearch.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="input-search-container">
-    <input
+    <b-form-input
       ref="input"
-      type="text"
       :placeholder="placeholder"
-      :class="['input-search', classes]"
-      :style="customStyle"
       :value="value"
-      @input="updateValue($event.target.value)"
-      autocomplete="off"
+      @input="updateValue"
+      class="input-search"
     />
 
     <button class="btn-search" @click="clearInputValue">
@@ -25,82 +22,69 @@ export default {
   props: {
     value: { type: String, require: true },
     placeholder: { type: String, default: "Pesquisar..." },
-    classes: { type: String, default: "" },
-    customStyle: { type: String, default: "" },
   },
+
   methods: {
     updateValue(newValue, wait = 400) {
-      debounce(() => {
-        this.$emit("input", newValue);
-      }, wait);
+      debounce(() => this.$emit("input", newValue), wait);
     },
     clearInputValue() {
       this.updateValue("", 0);
-      this.$refs.input.value = "";
       this.$refs.input.focus();
     },
   },
 };
 </script>
+
 <style lang="scss" scoped>
 @import "@/assets/styles/theme";
 
 .input-search-container {
   position: relative;
   width: 100%;
-}
-.input-search-container > .input-search {
-  width: 100%;
-  height: 25px;
-  font-size: 12px;
-  color: $clr-darkgray;
-  padding: 5px;
-  padding-right: 38px;
-  border: 1px solid #949494aa;
-  background-color: #fff;
-  border-radius: 4px;
-}
-.input-search-container > .input-search:focus {
-  box-shadow: none;
-  outline: none;
-  box-shadow: 0 0 0 0.1rem rgba(0, 123, 255, 0.25) !important;
-}
 
-.input-search-container > .btn-search {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  right: 1px;
-  top: 1px;
-  width: 35px;
-  height: calc(100% - 2px);
-  font-size: 12px;
-  padding: 0;
-  color: $clr-darkgray;
-  background-color: #fff;
-  border: 0;
-  border-top-right-radius: 4px;
-  border-bottom-right-radius: 4px;
-  border-left: 1px solid #949494aa;
-  cursor: pointer;
-  transition: all 0.2s ease;
-}
-.input-search-container > .btn-search:hover {
-  background-color: $clr-lightgray;
-}
+  > .input-search {
+    width: 100%;
+    height: 25px;
+    font-size: 12px;
+    color: $clr-darkgray;
+    padding: 5px;
+    padding-right: 38px;
 
-.input-search-container > .btn-search:focus {
-  box-shadow: none;
-  outline: none;
-}
+    &:focus {
+      box-shadow: none;
+      outline: none;
+      box-shadow: 0 0 0 0.1rem rgba(0, 123, 255, 0.25) !important;
+    }
+  }
 
-.btn-search,
-.input-search {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  -ms-appearance: none;
-  -o-appearance: none;
-  appearance: none;
+  > .btn-search {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    right: 1px;
+    top: 1px;
+    width: 35px;
+    height: calc(100% - 2px);
+    font-size: 12px;
+    padding: 0;
+    color: $clr-darkgray;
+    background-color: #fff;
+    border: 0;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border-left: 1px solid #949494aa;
+    transition: all 0.2s ease;
+
+    &:hover {
+      cursor: pointer;
+      background-color: $clr-lightgray;
+    }
+    &:focus {
+      box-shadow: none !important;
+      outline: none;
+    }
+  }
 }
 </style>
